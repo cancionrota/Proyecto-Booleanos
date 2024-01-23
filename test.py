352f8d3b02ff838a4033826e1b9eab29ae3d2109
Usuario Genérico Aulas - Libres <aml@pucp.edu.pe>
2024-01-23 11:23:25 -0500

    hola

--- test.py
+++ test.py
@@ -1,1 +1,10 @@
 print('miau')
+
+# 0. Clonas el repositorio
+# 1. Haces un cambio, guardas
+# 2. cd repositorio
+# 3. git add .
+# 4. git commit -m ""
+# 5. git push
+# 6. La otra compu: cd repositorio
+# 7. git pull origin main
